c843160dd04f87f9a336addea4b0d2c3706f196a
Matthew Marks <matthewfmarks@gmail.com>
2017-09-11 13:22:03 -0400

    combine `create table` statements
    wasn't working any other way

--- resources/migrations/20160221190951-add-users-table.up.sql
+++ resources/migrations/20160221190951-add-users-table.up.sql
@@ -7,3 +7,12 @@ CREATE TABLE if not exists conus
  imageurl VARCHAR(200),
  email VARCHAR(200),
  timestamp TIMESTAMP);
+--;;
+CREATE TABLE if not exists users
+(id INTEGER PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(200),
+githubid VARCHAR(200),
+email VARCHAR(200),
+login VARCHAR(200),
+location VARCHAR(200),
+timestamp TIMESTAMP);
