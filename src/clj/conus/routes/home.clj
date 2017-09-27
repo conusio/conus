050e62a2d2955a565f6f800f14faf57fb57204c9
elliott <elliottw@gmail.com>
2017-09-26 21:00:04 -0400

    things with no names get assigned random name and url

--- src/clj/conus/routes/home.clj
+++ src/clj/conus/routes/home.clj
@@ -64,9 +64,14 @@
     (st/validate params message-schema)))
 
 (defn fix-params [params random-prefix]
+  (let [_ (log/info "poo:" (clojure.string/trim (:name params)))])
   (as-> params $
-    (assoc $ :imageurl (if (not (empty? (get-in params [:file :filename]))) (str "/images/" random-prefix (get-in params [:file :filename]))))
-    (assoc $ :name (clojure.string/trim (:name $)))))
+    (assoc $ :imageurl (if (not (empty? (get-in params [:file :filename])))
+                         (str "/images/" random-prefix (get-in params [:file :filename]))))
+    (assoc $ :name (if (not (empty? (clojure.string/trim (:name $))))
+                     (clojure.string/trim (:name $))
+                     (str random-prefix "untitled")
+                     ))))
 
 (defn upload-file-helper! [params random-prefix]
   (when (not= "" (get-in params [:file :filename])) (upload-file! resource-path (:file params) random-prefix)))
