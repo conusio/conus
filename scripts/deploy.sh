e06b5fb9d131f5e3bf6dc6a42cf7418aa789ff57
Matthew Marks on digitalocean <matthewfmarks@gmail.com>
2017-09-29 00:56:07 +0000

    script pointed to old directory

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-nohup java -cp conus.jar:resources conus.core > foo.out 2> foo.err  & 
+nohup java -cp target/uberjar/conus.jar:resources conus.core > foo.out 2> foo.err  & 
 disown %
